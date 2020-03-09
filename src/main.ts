2ea57be6c5345c26c93a4af4241e2764ad129f5d
Jason Wedderburn <jasonwj96@gmail.com>
2020-03-08 21:37:58 -0500

    Added font-awesome

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,18 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+// @ts-ignore
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 Vue.config.productionTip = false;
+
+library.add(faUserSecret);
+library.add(faCoffee);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
   router,
